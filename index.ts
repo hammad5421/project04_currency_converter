4b77460e4d44540aed51df2a316f705b05f940a0
Hammad Ahmad <139546876+hammad5421@users.noreply.github.com>
2023-10-21 11:18:44 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,60 @@
+import inquirer from "inquirer";
+
+let conversion = {
+  "PKR": {
+    "PKR": 1,
+    "USD": 0.0036,
+    "SAR": 0.013,
+  },
+  "USD": {
+    "PKR": 279,
+    "USD": 1,
+    "SAR": 3.75,
+  },
+  "SAR": {
+    "PKR": 73.66,
+    "USD": 0.27,
+    "SAR": 1,
+  },
+};
+
+interface CurrencyType {
+  from: "PKR" | "SAR" | "USD";
+  to: "PKR" | "SAR" | "USD";
+  amount: number;
+}
+
+async function main() {
+  const answers: CurrencyType = await inquirer.prompt([
+    {
+      type: "list",
+      name: "from",
+      choices: ["PKR", "SAR", "USD"],
+      message: "Enter your currency",
+    },
+    {
+      type: "list",
+      name: "to",
+      choices: ["PKR", "SAR", "USD"],
+      message: "Enter your conversion currency",
+    },
+    {
+      type: "number",
+      name: "amount",
+      message: "Enter your amount",
+    },
+  ]);
+
+  let { from, to, amount } = answers;
+
+  if (from && to && amount) {
+    let result = conversion[from][to] * amount;
+    console.log(
+      `The amount you entered: ${amount} ${from} is converted to ${result} ${to}`
+    );
+  } else {
+    console.log(`Invalid inputs`);
+  }
+}
+
+main();
